6c916201c4a837ccff37417234875e83d7c61f50
ankit-dmt <ankit@datamatter.tech>
2024-09-12 23:14:18 +0530

    modified added upcomin-workshop and career pages

---
+++ main.js
@@ -0,0 +1,5 @@
+  // Toggle the mobile menu when hamburger is clicked
+  document.getElementById("hamburger").addEventListener("click", function() {
+    var menu = document.getElementById("mobileMenu");
+    menu.classList.toggle("hidden");
+  });
